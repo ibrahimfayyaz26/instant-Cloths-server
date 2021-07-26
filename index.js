5f68718bf46413d8ca91bcbf9c920f1d37c1a1ba
Ibrahim <ibrahimfayyaz26@gmail.com>
2021-07-26 17:14:10 +0500

    server initialized

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-var Express = require("express"); //old syntax
+const Express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
 const app = Express();
 
-app.get("/123", (req, res) => {
-  res.send("it worked");
+//.env
+const User = process.env.ADMIN;
+const Pass = process.env.PASS;
+
+//main api
+app.get("/", (req, res) => {
+  res.send("Instant Cloths Server");
 });
 
-app.listen(100, () => {
-  console.log("app listening on port 3000");
-});
+//Database connection
+mongoose
+  .connect(
+    `mongodb+srv://${User}:${Pass}@cluster0.abewd.mongodb.net/instantCloths?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("app listening on port 3000");
+    });
+  })
+  .catch((err) => console.log(err));
