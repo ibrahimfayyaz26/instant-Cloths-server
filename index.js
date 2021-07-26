b4491008ed8b0f72aaa3290ce8b2af7d9ddc9d8e
Ibrahim <ibrahimfayyaz26@gmail.com>
2021-07-26 16:50:00 +0500

    oo

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("server");
+var Express = require("express"); //old syntax
+const app = Express();
+
+app.get("/123", (req, res) => {
+  res.send("it worked");
+});
+
+app.listen(100, () => {
+  console.log("app listening on port 3000");
+});
