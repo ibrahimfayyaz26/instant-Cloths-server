08eb59d94fa35171485acc133ccf91d52e5fb968
charged ahead <roshan-ican>
2021-07-31 22:42:13 +0530

    guess done finally

--- routes/shop.js
+++ routes/shop.js
@@ -2,6 +2,35 @@ const express = require("express");
 const router = express.Router();
 const Shop = require("../models/Shop");
 
+const multer = require("multer");
+
+//Multer
+const file_type = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpeg",
+  "image/png": "png",
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cd) => {
+    const fileT = file_type[file.mimetype];
+    let err = new Error("wrong image type");
+    if (fileT) {
+      err = null;
+    }
+    cd(err, "uploads");
+  },
+  filename: (req, file, cd) => {
+    const fileT = file_type[file.mimetype];
+    const fileName = `${file.originalname
+      .split(" ")
+      .join()}-${Date.now()}.${fileT}`;
+    cd(null, fileName);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 //get all shops
 router.get("/", async (req, res) => {
   try {
@@ -22,7 +51,15 @@ router.get("/:id", async (req, res) => {
 });
 
 //create shop
-router.post("/", async (req, res) => {
+router.post("/", upload.single("images"), async (req, res) => {
+  let imagesFormat = [];
+
+  req.file.map((fileT) => {
+    imagesFormat.push(
+      `${req.protocol}://${req.get("host")}/uploads/${fileT.fileName}`
+    );
+  });
+
   const shop = new Shop({
     name: req.body.name,
     image: req.body.image,
