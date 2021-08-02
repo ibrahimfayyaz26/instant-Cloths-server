e3e340dd4d2649a0713e1f4d7a2e85fb292c294d
charged ahead <roshan-ican>
2021-08-02 20:29:08 +0530

    trying to get it right

--- routes/chat.js
+++ routes/chat.js
@@ -4,15 +4,21 @@ const bodyParser = require('body-parser')
 const Pusher = require('pusher')
 const Shop = require("../models/Chat");
 
-app.use(bodyParser.json())
+
+const Pusher = require("pusher");
 
 const pusher = new Pusher({
-  app_id = "1244104",
-  key = "d140d47811bd10da850c",
-  secret = "7a83d9f32574b7432eb8",
-  cluster = "ap2",
-  encrypted: true
-})
+  appId: "1244104",
+  key: "d140d47811bd10da850c",
+  secret: "7a83d9f32574b7432eb8",
+  cluster: "ap2",
+  useTLS: true
+});
+
+pusher.trigger("my-channel", "my-event", {
+  message: "hello world"
+});
+
 
 //set cors middleware
 app.use(function(req, res, next) {
